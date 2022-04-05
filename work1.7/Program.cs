6a664cc3c02edee5cb2bce648d3c7f99b54e57b1
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-05 19:30:53 +0300

    init

---
+++ work1.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace work1._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInLine;
+            int expectationLine = 10;
+            int expectationInHours;
+            int expectationInMinutes;
+
+            Console.Write("Введите кол-во людей в очереди: ");
+            peopleInLine = Convert.ToInt32(Console.ReadLine());
+
+            expectationInHours = (expectationLine * peopleInLine) / 60;
+            expectationInMinutes = (expectationLine * peopleInLine) % 60;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {expectationInHours} часа и {expectationInMinutes} минут.");
+        }
+    }
+}
