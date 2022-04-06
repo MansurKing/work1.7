86b74c92dd5ae148840a013cc5a12e616fff9d60
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-06 17:45:41 +0300

    init

--- work1.7/Program.cs
+++ work1.7/Program.cs
@@ -10,12 +10,13 @@ namespace work1._7
             int expectationLine = 10;
             int expectationInHours;
             int expectationInMinutes;
+            int isOneHour = 60;
 
             Console.Write("Введите кол-во людей в очереди: ");
             peopleInLine = Convert.ToInt32(Console.ReadLine());
 
-            expectationInHours = (expectationLine * peopleInLine) / 60;
-            expectationInMinutes = (expectationLine * peopleInLine) % 60;
+            expectationInHours = (expectationLine * peopleInLine) / isOneHour;
+            expectationInMinutes = (expectationLine * peopleInLine) % isOneHour;
 
             Console.WriteLine($"Вы должны отстоять в очереди {expectationInHours} часа и {expectationInMinutes} минут.");
         }
